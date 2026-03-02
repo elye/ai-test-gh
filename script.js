7db0cfd83bb9f32073d910c5c6745be67617ca32
elye <elye.project@gmail.com>
2026-03-02 18:59:20 +1100

    Add datetime page and link from main page

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.getElementById('greet').addEventListener('click', function(){
   alert('Hello, world!');
   console.log('Hello, world!');
 });
+
+var dtBtn = document.getElementById('open-datetime');
+if(dtBtn){
+  dtBtn.addEventListener('click', function(){
+    window.location.href = 'datetime.html';
+  });
+}
